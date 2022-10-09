8ebb3ef1bfd4b842a62ba89e6e3299cc59454f56
Hinara Pastora Sánchez Mata <69545933+HinaraSM@users.noreply.github.com>
2022-10-09 01:20:44 -0500

    Update Fabula.java

--- src/main/java/comunicacion/Fabula.java
+++ src/main/java/comunicacion/Fabula.java
@@ -33,7 +33,7 @@ public class Fabula extends Escrito{
 
     @Override
     public int palabrasTotales(int palabrasPagina){
-        return palabrasPagina*1;
+        return palabrasPagina*getPaginas()*1;
     }
 
     @Override
